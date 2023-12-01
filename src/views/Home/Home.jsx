726a22270d6d6254920ab3579af7b582f9ee5429
Melodi Balossino <melodibalossino@gmail.com>
2023-11-30 23:39:05 -0300

    React-icons

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,8 +1,12 @@
 import Slider from "../../components/Slider/Slider";
+import Footer from "../../components/Footer/Footer";
 
 const Home = () => {
     return (
+        <>
         <Slider />
+        <Footer />
+        </>
     )
 }
 
